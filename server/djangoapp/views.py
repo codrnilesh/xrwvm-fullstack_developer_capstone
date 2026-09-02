b24c7be41c99136d8f77e6e3b35ff2fbce8556e2
Nilesh Shewale <210632129+codrnilesh@users.noreply.github.com>
2026-09-02 19:20:09 -0400

    Add CarMake and CarModel models

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -15,6 +15,7 @@ import json
 from django.views.decorators.csrf import csrf_exempt
 from .populate import initiate
 
+from .models import CarMake, CarModel # manually added
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
@@ -38,7 +39,6 @@ def login_user(request):
         data = {"userName": username, "status": "Authenticated"}
     return JsonResponse(data)
 
-# Create a `logout_request` view to handle sign out request
 # Create a `logout_request` view to handle sign out request
 def logout_request(request):
     logout(request)
@@ -79,6 +79,23 @@ def registration(request):
         data = {"userName":username,"error":"Already Registered"}
         return JsonResponse(data)
 
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+    print(count)
+
+    if count == 0:
+        initiate()
+
+    car_models = CarModel.objects.select_related('car_make')
+    cars = []
+
+    for car_model in car_models:
+        cars.append({
+            "CarModel": car_model.name,
+            "CarMake": car_model.car_make.name
+        })
+
+    return JsonResponse({"CarModels": cars})
 
 # # Update the `get_dealerships` view to render the index page with
 # a list of dealerships
